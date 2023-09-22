b64386bbfdb1081b89ad243130f30721372d01ae
Gurbinder Singh <gurbinder.singh.2sma2@simplelogin.com>
2023-09-22 20:50:13 +0200

    Update trash script

--- bash/trash.sh
+++ bash/trash.sh
@@ -1,12 +1,70 @@
 #!/bin/bash
+set -e
 
-# TODO: Add options to list and clear trash using getopts
+usage() {
+  echo "Usage: trash.sh [OPTIONS] [FILE...]"
+  echo ""
+  echo "  Option:"
+  echo "    -e  Empty bin"
+  echo "    -l  List files in bin"
+  echo "    -h  Show help"
+  exit "$1"
+}
+
+
+error() {
+  echo "$1" >&2
+  exit "$2"
+}
+
+
+
+list_files() {
+  echo "Files in trash bin:"
+  ls -lh "$trashcan"
+  exit 0
+}
+
+
+empty_bin() {
+  file_count="$(ls -1 | wc -l | tr -d ' ')"
+  read -rp "Empty bin ($file_count files will be deleted)? (y/n) " confirm
+  
+  if [[ $confirm =~ [yY] ]]; then
+    echo "Confirmed"
+  fi
+  exit 0
+}
+
 
 if [[ $# -lt 1 ]]; then
-  echo "Missing parameter"
-  echo "Usage: trash.sh [OPTIONS] FILE..."
-  exit 1
+  usage 1
 fi
 
+
 trashcan="$HOME/.trash-can/"
-mv -v "$@" "$trashcan"
+optstring=':hle'
+
+
+while getopts "${optstring}" arg; do
+  case ${arg} in
+    e)
+      empty_bin
+      ;;
+    h)
+      usage 0
+      ;;
+    l)
+      list_files
+      ;;
+    :)
+      error "Option -$OPTARG expects an argument." 2
+      ;;
+    ?)
+      error "Invalid option: -${OPTARG}." 3
+      ;;
+  esac
+done
+
+echo "Here"
+# mv -v "$@" "$trashcan"
