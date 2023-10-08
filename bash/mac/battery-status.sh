1c0b2cff913237ad0364597487415d3a7756cbb1
Gurbinder Singh <gurbinder.singh.2sma2@simplelogin.com>
2023-10-08 15:52:04 +0200

    Update script

--- bash/mac/battery-status.sh
+++ bash/mac/battery-status.sh
@@ -3,3 +3,8 @@
 logFile="$HOME/logs/battery-status.log"
 status="$(pmset -g ps | grep -Eo '[0-9]{1,3}%; (dis)?charging')"
 echo "[$(date +'%F %H:%M:%S')] Battery at $status." >> "$logFile" 2>&1
+
+num="$(echo "$status" | grep -Eo '[0-9]{1,3}')"
+if (( $num < 30 || $num > 70)); then
+  shortcuts run "Battery Level" > /dev/null
+fi
