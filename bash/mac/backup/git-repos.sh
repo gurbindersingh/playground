0194fb4ab4ace68f2fc87b7d46b14afee489a2bf
Gurbinder Singh <gurbinder.singh.2sma2@simplelogin.com>
2023-11-17 09:28:37 +0100

    Add new backup script

---
+++ bash/mac/backup/git-repos.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+directory="$(dirname -- "${BASH_SOURCE[0]}")"
+cd "$directory" || exit 1
+
+dest="$(cat repos-dest.txt)"
+readarray -t repos < "repos.txt"
+
+for repo in "${repos[@]}"; do
+  echo "[$(date +'%F %H:%M:%S')] Creating backup of '$repo'"
+  rsync -av \
+    --exclude=".git" \
+    --exclude=".tmp.drive*" \
+    --exclude=".idea" \
+    --exclude="target" \
+    --exclude="*.ini" \
+    "$HOME/Developer/$repo" "$HOME/${dest:?}"
+    
+  echo ""
+  echo "[$(date +'%F %H:%M:%S')] DONE"
+  echo ""
+done
+
+
--- bash/mac/backup/run-all.sh
+++ bash/mac/backup/run-all.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 logFile="$HOME/logs/run-all-backups.log"
-scriptPath="$HOME/Developer/playground/bash/mac/backup"
+scriptPath="$(dirname -- "${BASH_SOURCE[0]}")"
 
 {
   bash "$scriptPath/vscode-settings.sh"
   bash "$scriptPath/firefox.sh"
   bash "$scriptPath/mac-settings.sh"
+  bash "$scriptPath/git-repos.sh"
   printf "\n\n"
 } >> "$logFile" 2>&1
