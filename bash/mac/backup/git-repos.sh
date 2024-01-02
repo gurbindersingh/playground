7b2ab135be71fac67adf855cc1f3c0e3e36e87d1
Gurbinder Singh <gurbinder.singh.2sma2@simplelogin.com>
2024-01-02 11:08:55 +0100

    Fix script for older bash version

--- bash/mac/backup/git-repos.sh
+++ bash/mac/backup/git-repos.sh
@@ -7,7 +7,13 @@ directory="$(dirname -- "${BASH_SOURCE[0]}")"
 cd "$directory" || exit 1
 
 dest="$(cat repos-dest.txt)"
-readarray -t repos < "repos.txt"
+# The preferred method for reading lines into an array should be `readarray` 
+# but on macOS if we run a script using launchd instead of cron the deprecated 
+# version of Bash (3.x) is used. This is a workaround for the older version.
+# The `-n` option of the test command checks if the string is non-zero.
+while IFS= read -r line || [ -n "$line" ]; do
+  repos+=("$line")
+done < "repos.txt"
 
 for repo in "${repos[@]}"; do
   echo "[$(date +'%F %H:%M:%S')] Creating backup of '$repo'"
