56271aeaf6948ecbff1432552a9275b927f426c3
Gurbinder Singh <gurbinder.singh.2sma2@simplelogin.com>
2024-02-25 14:33:46 +0100

    Add restart signal

--- bash/signal-watcher.sh
+++ bash/signal-watcher.sh
@@ -5,6 +5,7 @@ app_dir="$HOME/app"
 log_file="$app_dir/signal-watcher.log"
 stop_signal="signal.stop"
 start_signal="signal.start"
+restart_signal="signal.restart"
 
 {
   cd "$app_dir"
@@ -14,5 +15,9 @@ start_signal="signal.start"
   elif [[ -e $start_signal ]]; then
     bash start.sh
     rm -v $start_signal
+  elif [[ -e $restart_signal ]]; then
+    bash stop.sh
+    bash start.sh
+    rm -v $restart_signal
   fi
 } >> "$log_file" 2>&1
