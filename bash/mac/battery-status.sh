05bec46590f2b865fecc7ae9a9122bb7657f4ded
Gurbinder Singh <gurbinder.singh.2sma2@simplelogin.com>
2024-03-16 16:40:33 +0100

    Update script

--- bash/mac/battery-status.sh
+++ bash/mac/battery-status.sh
@@ -3,9 +3,9 @@ set -e
 
 logFile="$HOME/logs/battery-status.log"
 status="$(pmset -g ps | grep -Eo '[0-9]{1,3}%; (dis)?charg(ing|ed)')"
-num="$(echo "$status" | grep -Eo '[0-9]{1,3}')"
+percentage="$(echo "$status" | grep -Eo '[0-9]{1,3}')"
 
 echo "[$(date +'%F %H:%M:%S')] Battery at $status." >> "$logFile" 2>&1
-if (( num < 30 || num > 75)); then
-  shortcuts run "Battery Level" > /dev/null
+if (( percentage < 30 || percentage > 75)); then
+  osascript -e 'display notification "'"Battery at $status."'" with title "Battery status"'
 fi
