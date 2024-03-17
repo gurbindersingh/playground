4e0935c7a393d5f8649b57561521af25ac891775
Gurbinder Singh <gurbinder.singh.2sma2@simplelogin.com>
2024-03-17 20:07:17 +0100

    Update battery status script

--- bash/mac/battery-status.sh
+++ bash/mac/battery-status.sh
@@ -3,9 +3,10 @@ set -e
 
 logFile="$HOME/logs/battery-status.log"
 status="$(pmset -g ps | grep -Eo '[0-9]{1,3}%; (dis)?charg(ing|ed)')"
+state="$(echo "$status" | grep -Eo '[0-9]{1,3}%; (dis)?charg(ing|ed)')"
 percentage="$(echo "$status" | grep -Eo '[0-9]{1,3}')"
 
 echo "[$(date +'%F %H:%M:%S')] Battery at $status." >> "$logFile" 2>&1
-if (( percentage < 30 || percentage > 75)); then
+if [[ $state == 'discharging' && $percentage -lt 30 || $state == 'charging' && $percentage -gt 75 ]]; then
   osascript -e 'display notification "'"Battery at $status."'" with title "Battery status"'
 fi
