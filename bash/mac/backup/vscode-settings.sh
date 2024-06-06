5522159054eaf741832bf8d8eec86fcdd3357249
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2024-06-06 12:33:41 +0200

    Refactor script

--- bash/mac/backup/vscode-settings.sh
+++ bash/mac/backup/vscode-settings.sh
@@ -6,9 +6,10 @@ cd "$directory" || exit 1
 
 SRC="$HOME/Library/Application Support/Code"
 USER_SETTINGS="$SRC/User"
-DST="$HOME/$(cat configs/vscode-dest.txt)"
 
-echo "[$(date +'%F %H:%M:%S')] Creating backup of VS Code settings"
+. configs/vscode-dest.sh
+
+echo "[$(date +'%F %H:%M:%S')] Creating backup of VS Code settings at '$DST'"
 rsync -av "$USER_SETTINGS/settings.json" "$USER_SETTINGS/snippets" "$DST"
 rsync -av "$USER_SETTINGS/keybindings.json" "$DST/mac"
 echo "[$(date +'%F %H:%M:%S')] DONE"
