5b8af57e786bf4a1abd89d6b35789e709ad69cd8
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2024-09-28 09:32:30 +0200

    Update script

--- bash/mac/battery-status.sh
+++ bash/mac/battery-status.sh
@@ -8,9 +8,11 @@ state="$(echo "$status" | grep -Eo '(dis)?charg(ing|ed)')"
 percentage="$(echo "$status" | grep -Eo '[0-9]{1,3}')"
 message="Battery at ${percentage}%. ${state^}"
 
-# echo "here"
-if [[ $state == 'discharging' && $percentage -lt 20 || $state == 'charging' && $percentage -gt 80 ]]; then
-    osascript -e 'display dialog "'"$message."'" with title "Battery Status" buttons {"OK"} default button "OK"'
-    # osascript -e 'display notification "'"$message."'" with title "Battery Status"'
-fi
-echo "[$(date +'%F %H:%M:%S')] $message." >> "$logFile" 2>&1
+{
+    if [[ $state == 'discharging' && $percentage -lt 25 || $state == 'charging' && $percentage -gt 75 ]]; then
+        osascript -e 'display dialog "'"$message."'" with title "Battery Status" buttons {"OK"} default button "OK"'
+        # osascript -e 'display notification "'"$message."'" with title "Battery Status"'
+    fi
+    echo "[$(date +'%F %H:%M:%S')] $message." 
+} >> "$logFile" 2>&1
+
