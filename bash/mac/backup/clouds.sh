b84392f2b22929cadf2f6892111c990f0e9832a3
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2024-11-10 18:18:57 +0100

    Refactor cloud backup script
    Changes:
    - Archive modified files

--- bash/mac/backup/clouds.sh
+++ bash/mac/backup/clouds.sh
@@ -2,29 +2,37 @@
 set -e
 
 directory="$(dirname -- "${BASH_SOURCE[0]}")"
-cd "$directory" || exit 1
+cd "$directory"
 
-drive="/Volumes/Seagate"
 . configs/clouds.sh
 
-if [[ ! -e $drive ]]; then
-  echo "[ERROR] External drive $drive not found."
-  exit 1
+drive="/Volumes/${drive_name:?}"
+timestamp="$(date +'%F_%H%M.%S')"
+
+if [[ ! -e "${drive:?}" ]]; then
+    echo "[ERROR] External drive $drive not found."
+    exit 1
 fi
 
 for cloud in "${clouds[@]}"; do
-  source="$HOME/$cloud/"
-  destination="${drive:?}/$cloud/"
-  echo "[$(date +'%F %H:%M:%S')] Creating backup of '$source' at '$destination'."
-  rsync -avb \
-    --exclude=".git" \
-    --exclude=".tmp.drive*" \
-    --exclude=".idea" \
-    --exclude="target" \
-    --exclude="*.ini" \
-    --exclude="Sync.Cache" \
-    "$source" "$destination"
-    
-  echo "[$(date +'%F %H:%M:%S')] DONE"
-  echo ""
+    source="$HOME/$cloud/"
+    destination="$drive/$cloud/"
+
+    # NOTE: Incremental backups do not work on some file systems, like exFAT.
+    #       So instead we use the `--backup-dir` feature to create an archive 
+    #       of deleted and changed files.
+    echo "[$(date +'%F %H:%M:%S')] Creating backup of '$source' at '$destination'."
+    rsync -avb \
+        --delete-after \
+        --backup-dir="$drive/$cloud.changed/$timestamp" \
+        --exclude=".git" \
+        --exclude=".tmp.drive*" \
+        --exclude=".idea" \
+        --exclude="target" \
+        --exclude="*.ini" \
+        --exclude="Sync.Cache" \
+        "$source" "$destination"
+
+    echo "[$(date +'%F %H:%M:%S')] DONE"
+    echo ""
 done
