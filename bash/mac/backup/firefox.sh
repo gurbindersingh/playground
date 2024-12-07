b980eb39319fe7269274d85fb92da5b00c8d82ae
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2024-12-07 12:11:33 +0100

    Update backup script

--- bash/mac/backup/firefox.sh
+++ bash/mac/backup/firefox.sh
@@ -7,10 +7,15 @@ cd "$directory"
 . configs/firefox.sh
 
 source="$HOME/Library/Application Support/Firefox/Profiles"
-now="$(date +'%F.%H%M.%S')"
 
+# NOTE: Change into the source directory so that changes to the metadata in 
+#       the parent directories does not trigger a snapshot.
+cd "$source"
 echo "[$(date +'%F %H:%M:%S')] Creating backup of '$source' at '$dest'"
-rsync -av --delete-after --backup-dir="$now" "$source" "$HOME/$dest"
+restic backup --insecure-no-password --repo "$HOME/${dest:?}/backup" --skip-if-unchanged ./
 echo "[$(date +'%F %H:%M:%S')] DONE"
 echo "============================================================================="
 echo ""
+# NOTE:
+#   - List snapshots: restic snapshots --insecure-no-password --repo REPO
+#   - List files: restic ls SNAPSHOT_ID --insecure-no-password --repo REPO
