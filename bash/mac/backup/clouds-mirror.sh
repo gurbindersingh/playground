a81407f6cb86bc13da1b6b12310b704f62973ac4
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2025-01-18 10:59:27 +0100

    Backup clouds using restic intead of rsync

--- bash/mac/backup/clouds-mirror.sh
+++ bash/mac/backup/clouds-mirror.sh
@@ -8,32 +8,12 @@ cd "$directory"
 
 drive="/Volumes/${mirror_drive:?}"
 source_drive="/Volumes/${main_drive:?}"
-timestamp="$(date +'%F_%H%M.%S')"
 
 if [[ ! (-e "$drive" && -e "$source_drive") ]]; then
     echo "[ERROR] External drive $drive or $source_drive not found."
     exit 1
 fi
 
-for cloud in "${mirror_dirs[@]}"; do
-    source="$source_drive/$cloud/"
-    destination="$drive/$cloud/"
-
-    # NOTE: Incremental backups do not work on some file systems, like exFAT.
-    #       So instead we use the `--backup-dir` feature to create an archive 
-    #       of deleted and changed files.
-    echo "[$(date +'%F %H:%M:%S')] Creating backup of '$source' at '$destination'."
-    rsync -avb \
-        --backup-dir="$drive/$cloud.changed/$timestamp" \
-        --delete-after \
-        --exclude=".git" \
-        --exclude=".tmp.drive*" \
-        --exclude=".idea" \
-        --exclude="target" \
-        --exclude="*.ini" \
-        --exclude="Sync.Cache" \
-        "$source" "$destination"
-
-    echo "[$(date +'%F %H:%M:%S')] DONE"
-    echo ""
-done
+# restic --insecure-no-password init
+cd "$source_drive"
+restic --insecure-no-password backup --skip-if-unchanged ./
