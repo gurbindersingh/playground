0de27b2c1f9cb62dac76e394f16d61b5434bd295
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2025-03-09 12:31:09 +0100

    Refactor

--- bash/trash.sh
+++ bash/trash.sh
@@ -1,88 +1,79 @@
 #!/usr/bin/env bash
 set -e
 
-
 usage() {
-    echo "Usage: trash.sh [OPTIONS] [FILE...]"
-    echo ""
-    echo "  Option:"
-    echo "    -e  Empty bin"
-    echo "    -l  List files in bin"
-    echo "    -h  Show help"
-    exit "$1"
+  echo "Usage: trash.sh [OPTIONS] [FILE...]"
+  echo ""
+  echo "  Option:"
+  echo "    -e  Empty bin"
+  echo "    -l  List files in bin"
+  echo "    -h  Show help"
+  exit "$1"
 }
-
 
 error() {
-    echo "$1" >&2
-    usage "$2"
+  echo "$1" >&2
+  usage "$2"
 }
-
-
 
 list_files() {
-    echo "Files in trash bin:"
-    find "$trash_bin"
-    echo "Total size: $(du -hs "$trash_bin/")"
-    exit 0
+  echo "Files in trash bin:"
+  find "$trash_bin"
+  echo "Total size: $(du -hs "$trash_bin/")"
+  exit 0
 }
-
 
 empty_bin() {
-    file_count="$(find "$trash_bin" -type f | wc -l)"
+  file_count="$(find "$trash_bin" -type f | wc -l)"
 
-    if [[ $file_count -lt 1 ]]; then
-        echo "The trash bin is empty"
-        exit 0
-    fi
-
-    read -rp "Empty bin ($file_count files will be deleted)? (y/n) " confirm
-
-    if [[ $confirm =~ [yY] ]]; then
-        rm -rv "${trash_bin:?}"/*
-    fi
+  if [[ $file_count -lt 1 ]]; then
+    echo "The trash bin is empty"
     exit 0
-}
+  fi
 
+  read -rp "Empty bin ($file_count files will be deleted)? (y/n) " confirm
+
+  if [[ $confirm =~ [yY] ]]; then
+    rm -rv "${trash_bin:?}"/*
+  fi
+  exit 0
+}
 
 # =============================================================================
 # Main script
 # =============================================================================
 
 if [[ $# -lt 1 ]]; then
-    usage 1
+  usage 1
 fi
-
 
 trash_bin="$HOME/.trash-bin"
 options=':hle'
 
-
 if [[ ! -e "$trash_bin" ]]; then
-    echo "Creating $trash_bin"
-    mkdir -vp "$trash_bin"
+  echo "Creating $trash_bin"
+  mkdir -vp "$trash_bin"
 fi
 
 while getopts "${options}" arg; do
-    case ${arg} in
-        e)
-            empty_bin
-            ;;
-        h)
-            usage 0
-            ;;
-        l)
-            list_files
-            ;;
-        :)
-            error "Option -$OPTARG expects an argument." 2
-            ;;
-        ?)
-            error "Invalid option: -${OPTARG}." 3
-            ;;
-    esac
+  case ${arg} in
+  e)
+    empty_bin
+    ;;
+  h)
+    usage 0
+    ;;
+  l)
+    list_files
+    ;;
+  :)
+    error "Option -$OPTARG expects an argument." 2
+    ;;
+  ?)
+    error "Invalid option: -${OPTARG}." 3
+    ;;
+  esac
 done
-
 
 echo ""
 ls -1 "$@"
@@ -90,5 +81,7 @@ echo ""
 read -rp "Move the files above to trash bin? (y/n): " confirm
 echo ""
 if [[ $confirm =~ [yY] ]]; then
-    mv -v "$@" "$trash_bin"/
+  mv -v "$@" "$trash_bin"/
+  exit 0
 fi
+exit 1
