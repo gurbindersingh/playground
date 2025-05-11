dbbd7b2c7f798f9012bab79d12097d99b6af122d
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2025-05-11 18:11:26 +0200

    Exclude JAR files from backups

--- bash/mac/backup/jetbrains-configs.sh
+++ bash/mac/backup/jetbrains-configs.sh
@@ -5,10 +5,10 @@ directory="$(dirname -- "${BASH_SOURCE[0]}")"
 cd "$directory" || exit 1
 
 configs_dir="$HOME/Library/Application Support/JetBrains/"
-# Load config variables 
+# Load config variables
 . configs/jetbrains.sh
 
 for dest in "${targets[@]}"; do
-    echo "[$(date +'%F %H:%M:%S')] Backing up $configs_dir to $dest"
-    rsync -av --exclude='**.git' "$configs_dir" "$HOME/$dest"
+  echo "[$(date +'%F %H:%M:%S')] Backing up $configs_dir to $dest"
+  rsync -av --exclude='**.git' --exclude='**.jar' "$configs_dir" "$HOME/$dest"
 done
