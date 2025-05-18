8e74092e60c248aa292b667abbe53ccb8c90f27b
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2025-05-18 18:40:41 +0200

    Refactor script

--- bash/mac/backup/clouds/sync.sh
+++ bash/mac/backup/clouds/sync.sh
@@ -4,8 +4,6 @@ set -e
 directory="$(dirname -- "${BASH_SOURCE[0]}")"
 cd "$directory"
 
-# . ./configs/clouds.sh
-
 if [[ "$#" -lt 2 ]]; then
   echo "Missing parameter(s)"
   echo "Usage: sync SRC DEST"
@@ -18,12 +16,12 @@ destination="$2"
 timestamp="$(date +'%F_%H%M.%S')"
 backup_dir="$destination.changed/$timestamp"
 
-# NOTE: Incremental backups do not work on some file systems, like exFAT.
-#       So instead we use the `--backup-dir` feature to create an archive
-#       of deleted and changed files.
 echo "[$(date +'%F %H:%M:%S')] Creating backup of '$source' at '$destination'."
 echo "[$(date +'%F %H:%M:%S')] Changed and deleted files be saved at '$backup_dir'."
 
+# NOTE: Incremental backups do not work on some file systems, like exFAT.
+#       So instead we use the `--backup-dir` feature to create an archive
+#       of deleted and changed files.
 rsync -avb \
   --backup-dir="$backup_dir" \
   --delete-after \
