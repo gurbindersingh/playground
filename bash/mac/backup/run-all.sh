71f740641bb5144531bf78b50f186b6326fb5b0a
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2025-09-29 20:23:51 +0200

    Create backup script for Obsidian configs

--- bash/mac/backup/run-all.sh
+++ bash/mac/backup/run-all.sh
@@ -5,6 +5,7 @@ logFile="$HOME/logs/run-all-backups.log"
 {
   cd "$(dirname -- "${BASH_SOURCE[0]}")" || echo "[ERROR] Failed to cd." && exit 1
   bash jetbrains-configs.sh
+  bash obsidian-configs.sh
   bash firefox.sh
   bash repos.sh
   printf "\n\n"
