8e0c6fb003a9c5dd8dab2550a557283270c84a26
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2025-10-27 20:52:45 +0100

    Fix conditional execution

--- bash/mac/backup/run-all.sh
+++ bash/mac/backup/run-all.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
-
+set -e
 logFile="$HOME/logs/run-all-backups.log"
 
 {
-  cd "$(dirname -- "${BASH_SOURCE[0]}")" || echo "[ERROR] Failed to cd." && exit 1
+  echo "[DEBUG] Changing into script's directory."
+  cd "$(dirname -- "${BASH_SOURCE[0]}")" || {
+    echo "[ERROR] Failed to cd."
+    exit 1
+  }
+  set +e
+  echo "[INFO] Running backups"
   bash jetbrains-configs.sh
-  bash obsidian-configs.sh
   bash firefox.sh
   bash repos.sh
   printf "\n\n"
