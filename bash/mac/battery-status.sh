084945d724cd2cfccc52fbd86a73893f1b3a0378
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2025-10-27 20:42:55 +0100

    Remove redundant code

--- bash/mac/battery-status.sh
+++ bash/mac/battery-status.sh
@@ -11,7 +11,6 @@ message="Battery at ${percentage}%. ${state^}"
 {
   if [[ $state == 'discharging' && $percentage -le 30 || $state == 'charging' && $percentage -ge 70 && $percentage -lt 85 ]]; then
     osascript -e 'display dialog "'"$message."'" with title "Battery Status" buttons {"OK"} default button "OK"'
-    # osascript -e 'display notification "'"$message."'" with title "Battery Status"'
   fi
   echo "[$(date +'%F %H:%M:%S')] $message."
-} >>"$logFile" 2>&1
+} #>>"$logFile" 2>&1
