0969843df4af1fb0cd263eb640e6e279f96232d7
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2025-10-27 20:43:15 +0100

    Refactor

--- bash/mac/backup/repos.sh
+++ bash/mac/backup/repos.sh
@@ -8,21 +8,18 @@ cd "$directory" || exit 1
 
 . ./configs/repos.sh
 
-for repo in "${repos[@]}"; do
-  echo "[$(date +'%F %H:%M:%S')] Creating backup of '$repo'"
-  rsync -av \
-    --exclude=".git" \
-    --exclude=".tmp.drive*" \
-    --exclude=".idea" \
-    --exclude="target" \
-    --exclude="*.ini" \
-    --exclude="node_modules" \
-    --exclude=".venv" \
-    "$HOME/Developer/$repo" "$HOME/${dest:?}"
+echo "[$(date +'%F %H:%M:%S')] Creating backup of '$repos'"
+rsync -av \
+  --exclude=".tmp.drive*" \
+  --exclude=".idea" \
+  --exclude="target" \
+  --exclude="*.ini" \
+  --exclude="node_modules" \
+  --exclude=".venv" \
+  "$HOME/${repos:?}" "$HOME/${dest:?}"
 
-  echo ""
-  echo "[$(date +'%F %H:%M:%S')] DONE"
-  echo ""
-done
+echo ""
+echo "[$(date +'%F %H:%M:%S')] DONE"
+echo ""
 echo "============================================================================="
 echo ""
