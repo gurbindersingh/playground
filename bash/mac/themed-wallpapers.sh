b832163f5b0e5ddc86096128ef796ba9ea7fdba7
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2026-03-20 12:15:44 +0100

    Remove old code. Add arrays

--- bash/mac/themed-wallpapers.sh
+++ bash/mac/themed-wallpapers.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-directory="$(dirname -- "${BASH_SOURCE[0]}")"
-cd "$directory"
-
 now="$(date +'%T')"
+current_month=$(date +'%m')
 theme="dark"
-threshold="16:30:00"
 # Source: https://www.timeanddate.com/sun/austria/vienna
 # Longest day: 16:05 h, summer solstice, around 21st June.
 # Shortest day: 8:20 h, winter solstice, around 21st December.
@@ -14,31 +11,8 @@ threshold="16:30:00"
 # The following are civil twilight times. They are gave as a range on website,
 # so we use the larger of the two. Values rounded to the closest multiple of 5.
 # (The 13th entry in the array is simply so I don't have to some modulo gymnastics.)
-# sunrise=[0745 0720 0630 0630 0530 0455 0500 0535 0615 0655 0640 0725 0745]
-#  sunset=[1650 1730 1815 2000 2050 2130 2140 2100 2000 1900 1705 1635 1650]
+sunrise=(0745 0720 0630 0630 0530 0455 0500 0535 0615 0655 0640 0725 0745)
+sunset=(1650 1730 1815 2000 2050 2130 2140 2100 2000 1900 1705 1635 1650)
 
-if [[ "$now" < "$threshold" ]]; then
-  theme="light"
-fi
-# echo "$theme"
-
-currentWallpapers="$HOME/themed_wallpapers"
-allWallpapers="$HOME/$(cat wallpaper-location.txt)"
-currentTheme="$currentWallpapers/$theme"
-logFile="$HOME/logs/set-wallpapers.log"
-
-{
-  echo ""
-  if [[ -e "$currentTheme" ]]; then
-    echo "[$(date +'%F %H:%M:%S')] Nothing to do."
-    exit 0
-  fi
-  echo "[$(date +'%F %H:%M:%S')] Setting wallpapers for $theme theme."
-
-  rm -r "${currentWallpapers:?}/"
-  mkdir -p "$currentWallpapers"
-  ln "$allWallpapers/$theme"/* "$currentWallpapers"
-  touch "$currentWallpapers/$theme"
-
-  echo "[$(date +'%F %H:%M:%S')] Done."
-} >>"$logFile" 2>&1
+echo "$now"
+echo "$current_month"
