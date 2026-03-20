7f298609f026d4d8e0a428870108eb20ef6f0752
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2026-03-20 20:56:03 +0100

    Create function for calculating daylight interval

--- bash/mac/themed-wallpapers.sh
+++ bash/mac/themed-wallpapers.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-now="$(date +'%T')"
-current_month=$(date +'%m')
-theme="dark"
 # Source: https://www.timeanddate.com/sun/austria/vienna
 # Longest day: 16:05 h, summer solstice, around 21st June.
 # Shortest day: 8:20 h, winter solstice, around 21st December.
@@ -14,5 +11,25 @@ theme="dark"
 sunrise=(0745 0720 0630 0630 0530 0455 0500 0535 0615 0655 0640 0725 0745)
 sunset=(1650 1730 1815 2000 2050 2130 2140 2100 2000 1900 1705 1635 1650)
 
+# Fills the array passed as the first argument with the start and end of the daylight times.
+daylight_times() {
+  local -n result="${1:?}"
+  local current_month
+  current_month=$(date +'%m')
+  current_month=$((current_month - 1)) # Make the month an index for the array
+  # shellcheck disable=SC2034
+  result=("${sunrise[current_month]}" "${sunset[current_month]}")
+}
+
+daylight=()
+daylight_times daylight "$current_month"
+theme="dark"
+now="$(date +'%H%M')"
+
+if ((daylight[0] <= now && now <= daylight[1])); then
+  theme="light"
+fi
+
+echo "${daylight[@]}"
 echo "$now"
-echo "$current_month"
+echo "${theme:?}"
