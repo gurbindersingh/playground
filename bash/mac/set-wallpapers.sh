b2656c7725885e7f8d5a27ce9d6847e752826ee3
Gurbinder Singh <26441580+gurbindersingh@users.noreply.github.com>
2026-03-20 09:48:42 +0100

    Add more info

--- bash/mac/set-wallpapers.sh
+++ bash/mac/set-wallpapers.sh
@@ -11,8 +11,10 @@ threshold="16:30:00"
 # DST changes: last Sunday in March and October.
 # The following are civil twilight times. They are gave as a range on website,
 # so we use the larger of the two. Values rounded to the closest multiple of 5.
-# sunrise=[0745 0720 0630 0630 0530 0455 0500 0535 0615 0655 0640 0725]
-#  sunset=[1650 1730 1815 2000 2050 2130 2140 2100 2000 1900 1705 1635]
+# sunrise=[0745 0720 0630 0630 0530 0455 0500 0535 0615 0655 0640 0725 0745]
+#  sunset=[1650 1730 1815 2000 2050 2130 2140 2100 2000 1900 1705 1635 1650]
+# Longest day: 16:05 h, summer solstice, around 21st June.
+# Shortest day: 8:20 h, winter solstice, around 21st December.
 
 if [[ "$now" < "$threshold" ]]; then
   theme="light"
